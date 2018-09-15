b78a7c6495d1e4c5023ba5f081c0a4e45529a3a1
Gabriel Armas <Gabintosh1@gmail.com>
2018-09-15 16:17:31 -0500

    Clock 1.0!
    the first finalized version of the clock

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict'
+
+window.addEventListener('load',()=>{
+
+var clock = document.querySelector("#clock");
+
+setHour(clock);
+
+setInterval(()=>{
+setHour(clock);
+},1000);
+
+
+function setHour(t_clock) {
+	var date = new Date();
+    var hours = date.getHours();
+    var mins = date.getMinutes();
+    var secs = date.getSeconds();
+    
+    if(hours < 10)
+    	hours = "0" + hours.toString();
+    else if(mins < 10)
+    	mins = "0" + mins.toString();
+    else if(secs < 10)
+    secs = "0" + secs.toString();
+
+
+    t_clock.innerHTML = `${hours}:${mins}:${secs}`;
+   }
+
+
+});
+
+
+
